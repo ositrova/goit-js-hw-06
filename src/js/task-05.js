7dc137256f76ef0bdf6574090a6f74e5380424ed
ositrova <110326755+ositrova@users.noreply.github.com>
2022-12-10 14:34:55 +0200

    Update task-05.js

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,3 +1,19 @@
 // Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), 
 // підставляє його поточне значення в span#name-output. 
 // Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
+
+
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+
+input.addEventListener("input", (event) => {
+  if (event.currentTarget.value === '') {
+   return output.textContent = "Anonymous";
+  }
+    output.textContent = event.currentTarget.value;
+});
+
+console.log(input)
+
+console.log(output)
