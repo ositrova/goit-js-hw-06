a16b69faefbec931917714c77dda4b2c1fe3d553
ositrova <110326755+ositrova@users.noreply.github.com>
2022-12-11 08:59:38 +0200

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -8,3 +8,20 @@
 // зеленим, якщо неправильна кількість - червоним.
 // Для додавання стилів використовуй CSS-класи valid і invalid, які 
 // ми вже додали у вихідні файли завдання.
+
+
+const value = document.querySelector('#validation-input');
+const checkLength = value.dataset.length;
+
+value.addEventListener('blur', onBlur)
+
+function onBlur(event) {
+    if (event.currentTarget.value.length === Number(checkLength)) {
+      value.classList.remove('invalid');
+      value.classList.add('valid');
+    }
+    else {
+      value.classList.remove('valid');
+      value.classList.add('invalid');
+    }
+}
