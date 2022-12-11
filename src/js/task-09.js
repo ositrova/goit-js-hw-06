4bb4ed3ac47fe3e85892fecbe18df90e0cc43783
ositrova <110326755+ositrova@users.noreply.github.com>
2022-12-11 18:06:59 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -5,6 +5,17 @@
 // Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 
 
+const buttonEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const colorEl = document.querySelector('.color')
+
+buttonEl.addEventListener('click', event => {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  colorEl.textContent = color
+})
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
